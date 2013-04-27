730fb098f3558dce939a5f0b0b2c16559f55be98
Ehsan Sheikh <ehsan.sheikh@gmail.com>
2013-04-27 15:37:09 -0500

    all changes here too.

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from blog.models import Post
+
+def getRecentPosts(request):
+    #Get all blog posts
+    posts = Post.objects.all()
+
+    #Sort posts
+    sorted_posts = posts.order_by('-pub_date')
+
+    #Display all posts
+    return render_to_response('posts.html', { 'posts':sorted_posts})
