8afbc9b1ba838fcecc99c154275ead1f0c9e8deb
Ehsan Sheikh <ehsan.sheikh@gmail.com>
2013-04-27 14:48:39 -0500

    The directory

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    text = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
